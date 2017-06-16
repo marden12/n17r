ab3a83ff67f9204d06f4403ac4b5ab7396f90068
Dayana Tezekbaeva <mardentsk@gmail.com>
2017-06-16 17:57:41 +0600

    feat(.gitignore): add .gitignore

--- Pods/Target Support Files/Pods-mApp/Pods-mApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mApp : NSObject
-@end
-@implementation PodsDummy_Pods_mApp
-@end
--- Pods/Target Support Files/Pods-mAppTests/Pods-mAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_mAppTests
-@end
